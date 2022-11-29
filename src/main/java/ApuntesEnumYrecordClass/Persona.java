7c9620fbb5e9f2c2b39174f3b7f3b181b3c9db8d
Pablodaw2022 <pnavgarb@g.educaand.es>
2022-11-29 20:33:22 +0100

    Commit

---
+++ src/main/java/ApuntesEnumYrecordClass/Persona.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ApuntesEnumYrecordClass;
+
+/**
+ *
+ * @author samue
+ */
+public class Persona {
+    
+    public record  PersonaDTO (String nombre,String apellidos,int edad){}
+    
+}
